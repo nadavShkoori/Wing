2908e7565223cd465e564476dd97fcfade18a5c6
nadavShkoori <60702967+nadavShkoori@users.noreply.github.com>
2020-07-03 18:39:51 +0300

    comment initialization

--- src/app.js
+++ src/app.js
@@ -16,4 +16,4 @@ const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access Cloud Firestore.
 const admin = require('firebase-admin');
-admin.initializeApp();
+//admin.initializeApp();
