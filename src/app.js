78370fdc19a56cba41e73b343f793cd5777d0900
nadavShkoori <60702967+nadavShkoori@users.noreply.github.com>
2020-07-03 18:54:22 +0300

    comment firebase

--- src/app.js
+++ src/app.js
@@ -12,8 +12,8 @@ app.use(express.static('public'));
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require('firebase-functions');
+//const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access Cloud Firestore.
-const admin = require('firebase-admin');
+//const admin = require('firebase-admin');
 //admin.initializeApp();
